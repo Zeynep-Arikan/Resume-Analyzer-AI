7b0ab861af1f2f386042ddbca32478e3c812f2b4
Zeynep ARIKAN <zynparkn2110@gmail.com>
2025-04-19 08:36:10 +0300

    vectore-srore-command

--- vector_store.py
+++ vector_store.py
@@ -1,17 +1,17 @@
-import chromadb
-from chromadb.config import Settings
-from embedder import embed_text
+# import chromadb
+# from chromadb.config import Settings
+# from embedder import embed_text
 
-import chromadb
+# import chromadb
 
-chroma_client = chromadb.PersistentClient(path="db/chroma_store")
+# chroma_client = chromadb.PersistentClient(path="db/chroma_store")
 
 
-collection = chroma_client.get_or_create_collection(name="cv_database")
+# collection = chroma_client.get_or_create_collection(name="cv_database")
 
-def add_cv_to_vector_store(doc_id: str, text: str):
-    embedding = embed_text([text])[0]
-    collection.add(documents=[text], embeddings=[embedding], ids=[doc_id])
+# def add_cv_to_vector_store(doc_id: str, text: str):
+#     embedding = embed_text([text])[0]
+#     collection.add(documents=[text], embeddings=[embedding], ids=[doc_id])
 
 # def retrieve_similar_cvs(query: str, top_k=3):
 #     embedding = embed_text([query])[0]
